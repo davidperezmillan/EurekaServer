5ece2c696c3e6f38b9f24e92ecbf5ea61b98fc79
David Perez Millan <david.perez.millan@gmail.com>
2021-01-31 07:48:08 +0000

    commit inicial, proyecto basico

---
+++ src/main/java/com/nonave/dockerspringnetflix/DockerSpringNetflixEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.nonave.dockerspringnetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DockerSpringNetflixEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringNetflixEurekaServerApplication.class, args);
+	}
+
+}
