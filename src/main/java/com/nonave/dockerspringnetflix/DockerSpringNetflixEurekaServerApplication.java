24ead8445639944915b3fc9c93d6b5250deb424c
David Perez Millan <david.perez.millan@gmail.com>
2021-02-04 16:47:11 +0000

    Cambio desde consola

--- src/main/java/com/nonave/dockerspringnetflix/DockerSpringNetflixEurekaServerApplication.java
+++ src/main/java/com/nonave/dockerspringnetflix/DockerSpringNetflixEurekaServerApplication.java
@@ -3,9 +3,6 @@ package com.nonave.dockerspringnetflix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-
-
 @EnableEurekaServer
 @SpringBootApplication
 public class DockerSpringNetflixEurekaServerApplication {
