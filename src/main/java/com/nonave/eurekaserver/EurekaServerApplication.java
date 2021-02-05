bdbe30d9fce6d5be2c1570779718516cc72b4d25
david <david@david-AeroBook>
2021-02-05 11:09:03 +0100

    refactor project class main

--- src/main/java/com/nonave/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/nonave/eurekaserver/EurekaServerApplication.java
@@ -1,18 +1,14 @@
-package com.nonave.dockerspringnetflix;
+package com.nonave.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
-public class DockerSpringNetflixEurekaServerApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DockerSpringNetflixEurekaServerApplication.class, args);
-	}
-	
-	private static void info(){
-		System.out.println("Only test blue Ocean");
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
